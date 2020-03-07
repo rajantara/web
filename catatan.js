d88569cc309c5edf4b8dc44c87ea880b4efc016a
rajantara <rajanpensas@gmail.com>
2020-03-07 15:36:07 +0700

    add catatan.js

---
+++ catatan.js
@@ -0,0 +1,19 @@
+/*
+   
+                             |web tahap|
+
+1.searching di google express generator untuk membuka folder yang di inginkan
+2.npm instal pg -S
+3.setiap intal npm wajib mengunakan --save
+4.wajib instal npm insatll --save
+5.instal dulu semua npm yang di butuhkan pada package.json
+6.nodepostgreds untuk connect pada server
+7.npm run dev untuk nodemon aktif
+8.instal apk monggo
+9,instal pgadmin
+10.install postman
+
+
+
+
+*/
